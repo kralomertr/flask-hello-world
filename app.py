0e95f1b96ceb90681ee742ad10be7be13ba8c34d
kralomertr <omerfatihozdemir5@gmail.com>
2026-04-06 16:56:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
+import discord
+import os
 from flask import Flask
-app = Flask(__name__)
+from threading import Thread
 
+# Botun 7/24 açık kalması için küçük bir web sunucusu
+app = Flask('')
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home(): return "Bot Aktif!"
+
+def run(): app.run(host='0.0.0.0', port=8080)
+
+# Gerçek Discord Bot Kısmı
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print(f'{client.user} Olarak Giriş Yapıldı!')
+
+# Botu Başlat
+t = Thread(target=run)
+t.start()
+client.run(os.getenv('DISCORD_TOKEN'))
