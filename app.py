2232e35a1f74b1ea5705cbff18d83c4e0a61dc7b
kralomertr <omerfatihozdemir5@gmail.com>
2026-04-11 17:12:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,61 @@
+from flask.cli import load_dotenv
 import discord
+from discord.ext import commands
+from discord import app_commands
+import asyncio
 import os
-from flask import Flask
-from threading import Thread
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
-# Botun 7/24 açık kalması için küçük bir web sunucusu
-app = Flask('')
+class MyBot(commands.Bot):
+    def __init__(self):
+        # Botun temel izinlerini (intents) ayarlıyoruz
+        intents = discord.Intents.default()
+        super().__init__(command_prefix="!", intents=intents)
+
+    async def setup_hook(self):
+        # Komutları Discord'a global olarak kaydeder
+        await self.tree.sync()
+        print(f"Komutlar senkronize edildi!")
+
+bot = MyBot()
+
+# Birinci Komut: /komutlu
+@bot.tree.command(name="spam", description="Tek mesajın içinde 100 kez deneme yazar")
+async def spam(interaction: discord.Interaction):
+    
+    # 1'den 100'e kadar "deneme1", "deneme2" şeklinde bir liste oluşturup 
+    # aralarına boşluk koyarak tek bir uzun metin (string) yapıyoruz.
+    uzun_mesaj = " ".join([f"deneme {i+1}" for i in range(100)])
+    
+    try:
+        # Sadece 1 kere yanıt verdiğimiz için Discord bunu engellemez 
+        # ve bot sunucuda ekli olmasa bile herkes görebilir.
+        await interaction.response.send_message(uzun_mesaj)
+        
+    except Exception as e:
+        print(f"Hata oluştu: {e}")
+
+
+@bot.tree.command(name="ban", description="Belirlediğiniz üyeyi sunucudan yasaklar.")
+@app_commands.describe(kim="Yasaklanacak üyeyi seçin", sebep="Yasaklanma sebebi (isteğe bağlı)")
+@app_commands.checks.has_permissions(ban_members=False) # False yerine True olmalı
+async def ban(interaction: discord.Interaction, kim: discord.Member, sebep: str = "Belirtilmedi"):
+    try:
+        await kim.ban(reason=sebep)
+        await interaction.response.send_message(f"✅ **{kim.name}** başarıyla yasaklandı.\n**Sebep:** {sebep}", ephemeral=True)
+    except Exception as e:
+        await interaction.response.send_message(f"❌ Bir hata oluştu: {e}", ephemeral=True)
+
+
+# İkinci Komut: /komydynu
+@bot.tree.command(name="komydynu", description="Merhaba mesajı gönderir")
+async def komydynu(interaction: discord.Interaction):
+    await  interaction.response.send_message("islem başarili sunucuda veya kanalda açik vardir")
+
+# Botu başlat (Tokenini buraya yapıştır)
+bot.run(os.getenv("TOKEN"))
+
+
+# Botun 7/24 açık kalması 
 @app.route('/')
-def home(): return "Bot Aktif!"
-
-def run(): app.run(host='0.0.0.0', port=8080)
-
-# Gerçek Discord Bot Kısmı
-client = discord.Client(intents=discord.Intents.default())
-
-@client.event
-async def on_ready():
-    print(f'{client.user} Olarak Giriş Yapıldı!')
-
-# Botu Başlat
-t = Thread(target=run)
-t.start()
-client.run(os.getenv('DISCORD_TOKEN'))
